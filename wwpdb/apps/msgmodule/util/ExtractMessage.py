1457ce9e9de0fc3bb1d7f9f1f019013526c569a8
Ezra Peisach <ezra.peisach@rcsb.org>
2019-07-01 12:11:18 -0400

    Code cleanup

--- wwpdb/apps/msgmodule/util/ExtractMessage.py
+++ wwpdb/apps/msgmodule/util/ExtractMessage.py
@@ -16,9 +16,10 @@ __license__ = "Creative Commons Attribution 3.0 Unported"
 import os
 import sys
 import logging
-from wwpdb.utils.config.ConfigInfo import ConfigInfo, getSiteId
+from wwpdb.utils.config.ConfigInfo import getSiteId
 from mmcif.io.PdbxReader import PdbxReader
-from wwpdb.io.locator.PathInfo  import PathInfo
+from wwpdb.io.locator.PathInfo import PathInfo
+
 #
 
 logger = logging.getLogger(__name__)
@@ -32,26 +33,28 @@ class ExtractMessage(object):
         self.__pI = PathInfo(siteId=self.__siteId, verbose=self.__verbose, log=self.__log)
 
     def getLastUnlocked(self, depid):
-        """Returns datetime of the last time an unlocked message was sent.  This is parallel to the code in MessagingIo.py
+        """Returns datetime of the last time an unlocked message was sent.  This is parallel to the code in
+        MessagingIo.py
 
         If no unlock message has been sent, will return None
         """
 
-        msgfile = self.__pI.getFilePath(depid, contentType='messages-to-depositor', formatType='pdbx', fileSource="archive", versionId="1")
+        msgfile = self.__pI.getFilePath(depid, contentType='messages-to-depositor', formatType='pdbx',
+                                        fileSource="archive", versionId="1")
 
         if not os.path.exists(msgfile):
             return None
 
         ret = None
 
-        myContainerList=[]
+        myContainerList = []
         ifh = open(msgfile, "r")
-        pRd=PdbxReader(ifh)
+        pRd = PdbxReader(ifh)
         pRd.read(myContainerList)
 
         if len(myContainerList) >= 1:
-            c0=myContainerList[0]
-            catObj=c0.getObj("pdbx_deposition_message_info")
+            c0 = myContainerList[0]
+            catObj = c0.getObj("pdbx_deposition_message_info")
             if catObj is None:
                 logger.debug("Deposition %s no pdbx_deposition_message_info category" % depid)
                 return None
@@ -59,16 +62,16 @@ class ExtractMessage(object):
                 #
                 # Get column name index.
                 #
-                itDict={}
-                itNameList=catObj.getItemNameList()
-                for idxIt,itName in enumerate(itNameList):
-                    itDict[str(itName).lower()]=idxIt
+                itDict = {}
+                itNameList = catObj.getItemNameList()
+                for idxIt, itName in enumerate(itNameList):
+                    itDict[str(itName).lower()] = idxIt
                     #
-                idxOrdinalId=itDict['_pdbx_deposition_message_info.ordinal_id']
-                idxLastCommDate=itDict['_pdbx_deposition_message_info.timestamp']
-                idxMsgSubj=itDict['_pdbx_deposition_message_info.message_subject']
-                
-                maxUnlockOrdId=0
+                idxOrdinalId = itDict['_pdbx_deposition_message_info.ordinal_id']
+                idxLastCommDate = itDict['_pdbx_deposition_message_info.timestamp']
+                idxMsgSubj = itDict['_pdbx_deposition_message_info.message_subject']
+
+                maxUnlockOrdId = 0
                 for row in catObj.getRowList():
                     try:
                         ordinalId = int(row[idxOrdinalId])
@@ -82,11 +85,8 @@ class ExtractMessage(object):
                     except Exception as e:
                         logger.error("Error processing %s %s" % (depid, e))
 
-                
-        
         else:
             logger.debug("Deposition %s empty message file" % depid)
             return None
 
         return ret
-
