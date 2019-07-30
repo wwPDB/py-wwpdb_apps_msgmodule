9977d84dc25416ff0b76ad2697a392874202226c
Ezra Peisach <ezra.peisach@rcsb.org>
2019-07-30 13:15:39 -0400

    Split EMDB entries from PDB only ones to set flag for autoMsg invocation

--- wwpdb/apps/msgmodule/util/AutoMessage.py
+++ wwpdb/apps/msgmodule/util/AutoMessage.py
@@ -18,6 +18,8 @@ import logging
 from wwpdb.utils.config.ConfigInfo import ConfigInfo, getSiteId
 from wwpdb.apps.msgmodule.io.MessagingIo import MessagingIo
 from wwpdb.utils.session.WebRequest import InputRequest
+from mmcif.io.IoAdapterCore import IoAdapterCore
+from wwpdb.io.locator.PathInfo import PathInfo
 #
 
 logger = logging.getLogger(__name__)
@@ -49,5 +51,46 @@ class AutoMessage(object):
         return mio
 
     def sendRemindUnlocked(self, depidlist):
+        self._sendReminderBulk(depidlist, p_tmplt="remind-unlocked")
+
+    def _sendReminderBulk(self, depidlist, p_tmplt):
+        """Sends the bulk messages - handling setting EM flag"""
+
+        (pdbents, emdents) = self._splitents(depidlist)
+
         mio = self.__getmsgio()
-        mio.autoMsg(depidlist, p_tmpltType="remind-unlocked")
+        if pdbents:
+            mio.autoMsg(pdbents, p_tmpltType=p_tmplt)
+        if emdents:
+            mio.autoMsg(emdents, p_tmpltType=p_tmplt, p_isEmdbEntry=True)
+
+    def _getExptl(self, depid):
+        ctrgs = ['exptl']
+
+        ret = []
+        try:
+            pi = PathInfo(siteId=self.__siteId)
+            modelpath = pi.getModelPdbxFilePath(dataSetId=depid, fileSource="archive")
+            pr = IoAdapterCore()
+            containerl = pr.readFile(inputFilePath=modelpath, selectList=ctrgs)
+            if len(containerl) > 0:
+                block0 = containerl[0]
+                cobj = block0.getObj('exptl')
+                if cobj and cobj.hasAttribute('method'):
+                    for row in range(cobj.getRowCount()):
+                        ret.append(cobj.getValue('method', row))
+        except:
+            logger.exception("Failed to parse model file")
+
+        return ret
+
+    def _splitents(self, depidlist):
+        ements = []
+        pdbents = []
+        for depid in depidlist:
+            expmeths = self._getExptl(depid)
+            if 'ELECTRON MICROSCOPY' in expmeths:
+                ements.append(depid)
+            else:
+                pdbents.append(depid)
+        return (pdbents, ements)
